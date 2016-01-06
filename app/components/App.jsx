72074cd22de84eeb0abdb897f9501530b8436b6b
nonnycode <miknonny@gmail.com>
2016-01-06 09:36:41 +0100

    first component

---
+++ app/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Note from './Note.jsx';
+
+const { Component } = React;
+
+export default class App extends Component {
+  render () {
+    return <Note />
+  }
+}
---
+++ app/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const { Component } = React;
+
+export default class Note extends Component {
+  render () {
+    return <div>Learn something new</div>
+  }
+}
